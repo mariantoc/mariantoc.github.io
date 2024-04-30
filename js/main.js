5af6efa0930bbacc02219fb79fa0a52954fe5532
mariantoc <63203560+mariantoc@users.noreply.github.com>
2024-04-30 16:33:40 -0400

    Commit

--- js/main.js
+++ js/main.js
@@ -237,3 +237,19 @@
 
 })(jQuery);
 
+//Script slide show
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 1550); // Change image every 1.5 seconds
+}
